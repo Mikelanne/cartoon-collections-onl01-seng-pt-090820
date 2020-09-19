d8643889dc86fc0458f63abd6aa3d6c317fb7933
Mikelanne Wood <mikelanne.wood@gmail.com>
2020-09-19 20:31:17 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,8 +13,7 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array_of_calls)
- if array_of_calls.any? do |call|
-  call.length > 4
+ if array_of_calls.any? 
   true
    end
  end 
