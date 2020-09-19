05794d19571e83b5eb079c0fc0abf391419d1574
Mikelanne Wood <mikelanne.wood@gmail.com>
2020-09-19 20:29:52 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,29 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require 'pry'
+
+def roll_call_dwarves(dwarves)
+    dwarves.each.with_index(1) do |dwarf, index|
+    puts "#{index}. #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.map do |word|
+    "#{word.capitalize}!"
+  end 
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array_of_calls)
+ # if array_of_calls.any? do |call|
+  # call.length > 4
+ #  true
+ # end
+  # end 
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def find_the_cheese(array)
+ cheese_types = ["cheddar", "gouda", "camembert"]
+  if array.include? (cheese_types)
+    array.detect
+  end
+ # definitely use #detect.... but how?
 end
