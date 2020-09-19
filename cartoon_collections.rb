f55c58ca365676a711ab2c8d5e89a7917fa08dc6
Mikelanne Wood <mikelanne.wood@gmail.com>
2020-09-19 20:42:21 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,5 +18,5 @@ end
 
 def find_the_cheese(array)
  cheese_types = ["cheddar", "gouda", "camembert"]
-  array.detect {|cheese| array.include? (cheese_types)}
+  array.detect {|cheese| cheese.include? (cheese_types)}
 end 
